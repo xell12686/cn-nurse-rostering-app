97bc5ec7b0c102868c6dacaf14671a0b2dfa3d9c
jdev <jlacandula@gmail.com>
2024-05-13 05:55:19 +0800

    wip: rostering logic

--- src/common/rosterGenerator.ts
+++ src/common/rosterGenerator.ts
@@ -1,4 +1,5 @@
-import { Nurse, ShiftAssignment } from "@/types";
+import { Nurse, ShiftType, ShiftAssignment } from "@/types";
+import { selectNurseForShift, updateNurseSchedule } from "./nurseUtils";
 
 export function generateRoster(
   nurses: Nurse[],
@@ -8,7 +9,28 @@ export function generateRoster(
   const daysInMonth = new Date(year, month + 1, 0).getDate();
   const roster: ShiftAssignment[] = [];
 
-  // TODO: roster logic here...
+  for (let day = 1; day <= daysInMonth; day++) {
+    const date = new Date(year, month, day);
+    const dailyAssignment: ShiftAssignment = {
+      date,
+      shifts: {
+        Morning: [],
+        Evening: [],
+        Night: [],
+      },
+    };
+
+    ["Morning", "Evening", "Night"].forEach((shift) => {
+      while (dailyAssignment.shifts[shift].length < 5) {
+        const nurse = selectNurseForShift(nurses, shift as ShiftType, day);
+        if (!nurse) break; // Break if no available nurses
+        dailyAssignment.shifts[shift].push(nurse);
+        updateNurseSchedule(nurse, shift as ShiftType, day);
+      }
+    });
+
+    roster.push(dailyAssignment);
+  }
 
   return roster;
 }
