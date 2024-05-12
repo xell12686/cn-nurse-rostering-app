8a6ae0c6985a1e6ea97650fb5bad84984e55d4be
jdev <jlacandula@gmail.com>
2024-05-13 06:15:58 +0800

    feat: table like layout for MonthlyRoster

--- src/components/MonthlyRoster.tsx
+++ src/components/MonthlyRoster.tsx
@@ -12,15 +12,30 @@ const MonthlyRoster: React.FC<MonthlyRosterProps> = ({ nurses }) => {
     new Date().getMonth(),
     new Date().getFullYear(),
   );
-
+  console.log(roster);
+  
   return (
-    <div className="roster-items">
+    <div className="roster-items border m-10">
       {roster.map((day, index) => (
-        <div key={index} className="roster-item">
-          <h3>{day.date.toDateString()}</h3>
-          <div>Morning: {day.shifts.Morning.map((n) => n.name).join(", ")}</div>
-          <div>Evening: {day.shifts.Evening.map((n) => n.name).join(", ")}</div>
-          <div>Night: {day.shifts.Night.map((n) => n.name).join(", ")}</div>
+        <div
+          key={index}
+          className="roster-item border-b grid grid-cols-4 gap-3"
+        >
+          <div className="border-r p-2 flex items-center">
+            <h3>{day.date.toDateString()}</h3>
+          </div>
+          <div className="border-r p-2">
+            <div>Morning:</div>
+            <div>{day.shifts.Morning.map((n) => n.name).join(", ")}</div>
+          </div>
+          <div className="border-r p-2">
+            <div>Evening:</div>
+            <div>{day.shifts.Evening.map((n) => n.name).join(", ")}</div>
+          </div>
+          <div className="p-2">
+            <div>Night:</div>
+            <div>{day.shifts.Night.map((n) => n.name).join(", ")}</div>
+          </div>
         </div>
       ))}
     </div>
